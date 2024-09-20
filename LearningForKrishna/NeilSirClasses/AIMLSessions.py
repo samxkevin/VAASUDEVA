af08f6cefd4e3332286bf7af8c91d6d3e44e3c6e
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-09-20 11:48:51 +0530

    NeilSirClassUpdates

--- LearningForKrishna/NeilSirClasses/AIMLSessions.py
+++ LearningForKrishna/NeilSirClasses/AIMLSessions.py
@@ -3,3 +3,8 @@ these things together. AI is the thing which is parametrizing everything in the 
 Which will add us to a sensible equation. GPU's are fast matrix calculators so they are being used in the field of AI. Seamingly unconnected stuff is not really 
 unconnected until we have an equation. The world is going to a field called Hypercustomization.
 """
+"""
+Andrea Karparthy: He was the co-founder o open-AI. He was the CTO of AI division of Tesla. He made a series of zero to hero. It is worth it.
+ANN- Artificial Neural Network. Hidden Layers are also called as Black-Box. Gaussian Distribution. Probablity Distribution Function, Derivatives important topics
+in this case.  If we have to be great in AI we have to intimate with maths. We have to improve the grounding in math.
+"""
