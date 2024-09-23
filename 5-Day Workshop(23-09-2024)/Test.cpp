c6755b4f19d54a1ebaf1eab8e7fead97b243abb1
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-09-23 14:13:26 +0530

    Initial Commit for ProjectBMS

--- 5-Day Workshop(23-09-2024)/Test.cpp
+++ 5-Day Workshop(23-09-2024)/Test.cpp
@@ -1,5 +1,6 @@
 /*Is there a difference between Web and Internet
-The main difference between Internet and Web is that Internet is a connection of computers and the service that internet provides is known as Web.
-Web applications are a collection of webpages. WWW, Internet, Web Application, Website and how are all there interconnected.
- Mongo.db, express, react.js, node.js (MERN) for short is stack used for Web Development. Bootstrap is a library which is useful for designing a 
+The main difference between Internet and Web is that Internet is a connection of computers and the service that internet provides is known
+ as Web. Web applications are a collection of webpages. WWW, Internet, Web Application, Website and how are all there interconnected.
+ Mongo.db, express, react.js, node.js (MERN) for short is stack used for Web Development. Bootstrap is a library which is useful for designing 
+ a Responsive web app. 
 */
