276f0d10f27729c35dc3248a2c369fe0302b708a
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-09-24 06:33:43 +0530

    Solved sugarcane problem form cc and Pattern Problem in cpp

---
+++ C++/Codechef/sugarcane.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        cin>>n
+        cout<<n*15<<"\n";
+    }
+    return 0;
+}
--- LearningForKrishna/New_Things_Learnt.cpp
+++ LearningForKrishna/New_Things_Learnt.cpp
@@ -208,3 +208,22 @@ The above code block prints:
 ****
 *****
 */
+/* #include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n=5;
+  for(int j=1;j<=n;j++){
+  for(int i=1;i<=j;i++){
+    cout<<i;
+  }cout<<"\n";
+  }
+  return 0;
+} */
+/* 
+The above code block prints:
+1
+12
+123
+1234
+12345
+*/
