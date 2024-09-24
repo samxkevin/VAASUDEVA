572fb6ecc108cc85bc687cea32d209828241fc4c
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-09-24 05:46:46 +0530

    helium3 Codechef Problem Solved

---
+++ C++/Codechef/helium3.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,x,y;
+        cin>>a>>b>>x>>y;
+        if(a*b<=x*y)cout<<"yes\n";
+        else(cout<<"no\n");
+    }
+    return 0;
+}
