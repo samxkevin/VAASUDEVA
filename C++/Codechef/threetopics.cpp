643f3772fc16e737e66e0ac3d55368d4d76b2501
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-10-02 07:53:29 +0530

    edit in the last problem solved from cc

--- C++/Codechef/threetopics.cpp
+++ C++/Codechef/threetopics.cpp
@@ -1,13 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int a,b,c,x;
-        cin>>a>>b>>c>>x;
-        if(a==x||b==x||c==x)cout<<"yes\n";
-        else(cout<<"no\n");
-    }
+    int a,b,c,x;
+    cin>>a>>b>>c>>x;
+    if(a==x||b==x||c==x)cout<<"yes\n";
+    else(cout<<"no\n");
     return 0;
 }
