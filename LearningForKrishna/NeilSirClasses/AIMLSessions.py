ea0db03f7ab67c61d7a65477ce4d22737a66b4ff
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-10-04 12:48:41 +0530

    Update in Class

--- LearningForKrishna/NeilSirClasses/AIMLSessions.py
+++ LearningForKrishna/NeilSirClasses/AIMLSessions.py
@@ -4,9 +4,11 @@ Which will add us to a sensible equation. GPU's are fast matrix calculators so t
 unconnected until we have an equation. The world is going to a field called Hypercustomization.
 """
 """
-Andrej Karparthy: He was the co-founder o open-AI. He was the CTO of AI division of Tesla. He made a series of zero to hero. It is worth it.
+Andrej Karparthy: He was the co-founder of open-AI. He was the CTO of AI division of Tesla. He made a series of zero to hero. It is worth it.
 ANN- Artificial Neural Network. Hidden Layers are also called as Black-Box. Gaussian Distribution. Probablity Distribution Function, Derivatives important topics
-in this case.  If we have to be great in AI we have to intimate with maths. We have to improve the grounding in math. Intractable Problem: y has no relationship with x.
+in this case.  If we have to be great in AI we have to be intimate with maths. We have to improve the grounding in math. Intractable Problem: y has no relationship with x.
 We have an assumed function and training data for it. give input for that. compare that with the actual results. There will be errors. With that error, change the 
-parameters of the assumed function. Repeat this for accurate and precise results(EPOC). 
+parameters of the assumed function. Repeat this for accurate and precise results(EPOC).
 """
+"""Jupyter notebook is a python interpretor which is widely used in Deep Learning Applications. Loss Function- We will never get correct output.Initiallly we do not know the ways so we start with random weights which result in y now we give that x and predict that y which is in lot of variation with ground one. The difference between the actual value and the predicted value for y is called the Loss Variable. We use cross entropy for benign or malignity of the function for classification of the error.
+AI is an attempt to decrease the loss function to a bare minimum."""
--- LearningForKrishna/NeilSirClasses/zeroToHeroInterpretation.py
+++ LearningForKrishna/NeilSirClasses/zeroToHeroInterpretation.py
@@ -1,4 +1,4 @@
 #This Has been created upon the recommendation by Neil sir to go throught the Andrej Kaparthy video regarding Neural Networks.
 #Interpretation For the first video: The spelled-out intro to neural networks and backpropagation: building micrograd
-"""Jupiter notebooks are very important in this case. We are going to start with blank jupyter notebook. Micrograd is a library, a tiny Autograd engine (short term for 
+"""Jupyter notebooks are very important in this case. We are going to start with blank jupyter notebook. Micrograd is a library, a tiny Autograd engine (short term for 
 automamatic gradient) which implements backpropagation."""
