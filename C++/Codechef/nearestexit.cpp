d53ab562bbb03e10c1772f795f709b64073b8ebc
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-10-04 06:07:30 +0530

    nearest exit an d actemp solved

---
+++ C++/Codechef/nearestexit.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x>50)cout<<"RIGHT\n";
+        else(cout<<"LEFT\n");
+    }
+    return 0;
+}
