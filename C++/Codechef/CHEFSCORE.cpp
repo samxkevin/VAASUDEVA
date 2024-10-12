0a53283ee570cd15065b9f82f06470cf273fbe07
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-10-12 19:42:55 +0530

    solved test score from cc

--- C++/Codechef/CHEFSCORE.cpp
+++ C++/Codechef/CHEFSCORE.cpp
@@ -1,5 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x,y;
+        cin>>n>>x>>y;
+        if(y/x<=n&&y%x==0)cout<<"yes\n";
+        else(cout<<"no\n");
+    }
+    return 0;
 }
