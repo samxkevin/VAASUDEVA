81181072c7f3799d9350c7f803a7d2ab0f4b1c57
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-10-15 20:32:45 +0530

    jenga solved from cc

---
+++ C++/Codechef/JENGA.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        if(x%n==0)cout<<"yes\n";
+        else(cout<<"no\n");
+    }
+    return 0;
+}
