05c8a10f69510011deacf9c0614e62121472a4fc
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-10-17 15:12:19 +0530

    MAXTASTE solved cc

--- C++/Codechef/MAXTASTE.cpp
+++ C++/Codechef/MAXTASTE.cpp
@@ -4,11 +4,13 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int x,y,z;
-        cin>>x>>y>>z;
-        int max=x;
-        if(max<y)max=y;
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        int y=a+c,z=a+d,u=b+c,v=b+d;
+        int max=y;
         if(max<z)max=z;
+        if(max<u)max=u;
+        if(max<v)max=v;
         cout<<max<<"\n";
     }return 0;
 }
