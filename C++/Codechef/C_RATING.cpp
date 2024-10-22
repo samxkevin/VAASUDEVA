4be5f56641e1104e28cb9748d42b8ed61cd6a008
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-10-22 13:01:50 +0530

    C_RATING solved from cc

--- C++/Codechef/C_RATING.cpp
+++ C++/Codechef/C_RATING.cpp
@@ -1,9 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(0{
+int main(){
     int t;
     cin>>t;
     while(t--){
-        
+        int x,y;
+        cin>>x>>y;
+        if(x==y)cout<<0<<"\n";
+        else(cout<<((y-x+7)/8)<<"\n");
     }return 0;
-})
+}
